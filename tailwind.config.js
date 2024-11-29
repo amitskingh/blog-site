42c834aed4e39c8337eace876a7c40f57ef0221a
Amit Singh <amitsskingh@gmail.com>
2024-11-29 17:22:03 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    darkMode: "class", // or 'media' for automatic dark mode based on user settings
+    extend: {},
+  },
+  plugins: [],
+}
